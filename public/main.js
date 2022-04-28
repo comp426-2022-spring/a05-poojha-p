9dd495abd927426a3b96e7c09c32924f46377e39
Poojha Palle <poojhap@ad.unc.edu>
2022-04-27 22:21:07 -0400

    focus div

--- public/main.js
+++ public/main.js
@@ -1,6 +1,15 @@
 // Focus div based on nav button click
 
-document.getElementById("homenav").onclick = function(){
+function focusDiv(divname) {
+    var activeDivsCollection = document.getElementsByClassName("active");
+    var activeDivsArr = Array.from(activeDivsCollection)
+    activeDivsArr.forEach(function (currentdiv) {
+        currentdiv.setAttribute("class", "hidden");
+    })
+    document.getElementById(divname).setAttribute("class", "active");
+}
+
+/**document.getElementById("homenav").onclick = function(){
 
 }
 
@@ -14,7 +23,7 @@ document.getElementById("multinav").onclick = function(){
 
 document.getElementById("guessnav").onclick = function(){
 
-}
+}**/
 
 // Flip one coin and show coin image to match result when button clicked
 
@@ -26,6 +35,7 @@ function singleFlip(){
         document.getElementById("singleResult").innerHTML = result.flip;
         document.getElementById("singleResulimg").src = `./assets/img/${result.flip}.png`;  
     });
+}
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
